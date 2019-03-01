4e06990adaf857939e990aa102314a8340c594c2
Gabrielle Finear <gabriellefinear@Gabrielles-MacBook-Pro.local>
2019-03-01 14:44:30 -0500

    End of lesson 2 code. Added auth to app

--- snackhack-mobile/App.js
+++ snackhack-mobile/App.js
@@ -2,8 +2,14 @@ import React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { AppLoading, Asset, Font, Icon } from 'expo';
 import AppNavigator from './navigation/AppNavigator';
+import Auth from '@aws-amplify/auth'
+import awsconfig from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react-native';
+import Amplify from 'aws-amplify';
 
-export default class App extends React.Component {
+Amplify.configure(awsconfig);
+
+class App extends React.Component {
   state = {
     isLoadingComplete: false,
   };
@@ -60,3 +66,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
 });
+
+export default withAuthenticator(App, true);
